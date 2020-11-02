3f76ee0a33884a619c397e719f38d7271e4584ef
周健翔 <zhoujianxiang@evergrande.com>
2020-11-02 22:22:03 +0800

    错误页面错误

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">页面不存在！</h1>
+    <h1 v-else>系统应用错误</h1>
+    <nuxt-link to="/">返回首页</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  layout: "blank",
+  props: ["error"],
+};
+</script>
+<style></style>
