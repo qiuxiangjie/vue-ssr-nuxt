e6cc0b37cb6595602e5f6c80e9bdff0e1d0d6e79
zhoujianxiang <453139846@qq.com>
2020-11-17 15:34:05 +0800

    antd-ui按需加载

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -1,4 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @author: zhoujianxiang
+ * @Date: 2020-11-16 08:45:45
+ * @LastEditors: zhoujianxiang
+ * @LastEditTime: 2020-11-17 15:27:32
+ */
 import Vue from 'vue'
-import Antd from 'ant-design-vue/lib'
+//import Antd from 'ant-design-vue/lib'
+import { Button, Form, Input, Icon, Checkbox} from 'ant-design-vue';
+const components = [
+  Button,
+  Form,
+  Input,
+  Icon,
+  Checkbox
+]
 
-Vue.use(Antd)
+components.forEach(item => Vue.use(item))
+
