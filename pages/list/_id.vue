922f2f4bf64b016772239db2cb2a4f80fcdd67a6
zhoujianxiang <453139846@qq.com>
2020-11-18 14:41:09 +0800

    validate校验

--- pages/list/_id.vue
+++ pages/list/_id.vue
@@ -1,9 +1,14 @@
+
 <template>
   <div>detail:{{$route.params.id}}</div>
 </template>
 <script>
 export default {
   name: "",
+/*   validate({ params }) {
+    // 参数校验，失败自动跳到错误页面
+  return /^\d+$/.test(params.id)
+  } */
 };
 </script>
 <style></style>
